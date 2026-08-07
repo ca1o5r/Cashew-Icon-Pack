f896c9935f6a825788443fbe0316627c5a900cae
c0fle <ae20wre36@gmail.com>
2026-08-07 11:15:37 +0800

    refactor: remove 16 unused resource files, trim blueprint_setup.xml (v3.7.0)
    Deleted files verified unused via Blueprint/Frames bytecode analysis:
    - values/: dashboard_setup, dimens, floats, integers, docks, shape_styles, skin_colors
    - xml/: themecfg, themeinfo, appmap
    - menu/: drawer_menu
    - drawable/: ic_icons_preview
    - assets/: themecfg, themeinfo, app_func_theme, desk
    
    Trimmed blueprint_setup.xml to 4 values actually referenced:
      xml_drawable_enabled, includes_adaptive_icons,
      max_apps_to_request, time_limit_in_minutes

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.cashew.iconpack"
-    const val version = 280
-    const val versionName = "3.6.0"
+    const val version = 281
+    const val versionName = "3.7.0"
 }
